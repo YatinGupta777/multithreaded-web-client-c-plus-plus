8c6bd251b983029afe7bd62caee9cf9ad417b229
Yatin Gupta <yatingupta7777@gmail.com>
2022-09-18 17:16:43 -0500

    Fix critical section for parsing response

--- multithreaded-web-client/main.cpp
+++ multithreaded-web-client/main.cpp
@@ -128,8 +128,8 @@ void crawl(Parameters*p, char* link, HTMLParserBase*&parser) {
             }
 
             if (!obj.error) {
-                EnterCriticalSection(&statsCriticalSection);
                 int nlinks = obj.parse_response(original_link, parser);
+                EnterCriticalSection(&statsCriticalSection);
                 p->total_links_found += nlinks;
                 //TAMU hosts
                 if (strlen(host) >= 8 && strcmp(host + strlen(host) - 8, "tamu.edu") == 0) {
@@ -235,7 +235,7 @@ UINT stats_thread(LPVOID pParam)
     printf("Crawled %d pages @ %d/s (%.2f MB)\n", p->status_codes[0], p->status_codes[0] / elapsed_time, (float)p->total_bytes/1000000.0 );
     printf("Parsed %d links @ %d/s\n", p->total_links_found, p->total_links_found / elapsed_time);
     printf("HTTP codes: 2xx = %d, 3xx = %d, 4xx = %d, 5xx = %d, other = %d\n", p->status_codes[0], p->status_codes[1], p->status_codes[2], p->status_codes[3], p->status_codes[4]);
-    printf("TAMU Hosts links %d : %d, NON TAMU hosts links %d: %d", p->tamu_domain, p->total_tamu_links[0],p->non_tamu_domain, p->total_tamu_links[1]);
+    //printf("TAMU Hosts links %d : %d, NON TAMU hosts links %d: %d", p->tamu_domain, p->total_tamu_links[0],p->non_tamu_domain, p->total_tamu_links[1]);
     return 0;
 }
 
